fa9e9796be7715a3449b5c8d80fca676995dd24d
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2023-05-26 08:34:36 -0500

    Punto 1
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

---
+++ src/components/LoginForm.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+
+function LoginForm() {
+  const [authenticated, setAuthenticated] = useState(undefined);
+
+  function getErrorMessage() {
+    if (authenticated === false) {
+      return (
+        <p style={{ color: "red" }}>
+          Error de autenticación. Revise sus credenciales
+        </p>
+      );
+    }
+  }
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        login: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "error") {
+          setAuthenticated(false);
+        } else if (data.status === "success") {
+          setAuthenticated(true);
+          window.location.href = "/home";
+        }
+      });
+  }
+
+  return (
+    <Container>
+      <h3>Inicio de sesión</h3>
+      <Card className="d-flex justify-content-center login-card">
+        <Card.Body className="mx-auto" style={{ width: "75%" }}>
+          <Form onSubmit={handleLogin}>
+            <Form.Group controlId="username">
+              <Form.Label>Nombre de usuario</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <br />
+            <Row className="d-flex justify-content-center">
+              <Col md={6} className="d-flex justify-content-center">
+                <Button variant="success" type="submit" className="w-100">
+                  Iniciar sesión
+                </Button>
+              </Col>
+              <Col md={6} className="d-flex justify-content-center">
+                <Button variant="danger" type="reset" className="w-100">
+                  Cancelar
+                </Button>
+              </Col>
+            </Row>
+            <br />
+            <div className="d-flex justify-content-center">
+              {getErrorMessage()}
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Container fluid>
+    <BrowserRouter>
+      <h1 style={{ fontFamily: "fantasy"}}>El aroma mágico</h1> 
+      <hr/>
+      <Row>
+        <Col md={12}>
+          <Image src="coffe-banner.png" fluid width={"100%"} />
+        </Col>
+      </Row>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      <br/>
+      <footer className='d-flex justify-content-center'>
+        Contact us: +57 3102105253 - info@elaromamagico.com - @elaromamagico
+      </footer>
+    </BrowserRouter>
+  </Container>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
